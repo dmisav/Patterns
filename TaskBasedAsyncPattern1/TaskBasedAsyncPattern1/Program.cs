3c79eb1941cca8747668f3bcde911075543ebd29
dmisav <dmitry.savchenko@miratechgroup.com>
2014-07-31 10:54:35 +0300

    Added Task Based Async Pattern sample project

---
+++ TaskBasedAsyncPattern1/TaskBasedAsyncPattern1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TaskBasedAsyncPattern1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var list = new ConcurrentBag<string>();
+            string[] dirNames = { ".", ".." };
+            List<Task> tasks = new List<Task>();
+            foreach (var dirName in dirNames)
+            {
+                Task t = Task.Run(() =>
+                {
+                    SomeMethod(dirName, list);
+                });
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray());
+            foreach (Task t in tasks)
+                Console.WriteLine("Task {0} Status: {1}", t.Id, t.Status);
+
+            Console.WriteLine("Number of files read: {0}", list.Count);
+            Console.ReadLine();
+        }
+
+        private static void SomeMethod(string dirName, ConcurrentBag<string> list)
+        {
+            foreach (var path in Directory.GetFiles(dirName))
+                list.Add(path);
+        }
+    }
+}
